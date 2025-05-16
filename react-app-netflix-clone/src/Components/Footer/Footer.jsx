4fbf682dae428c546c57e321bf7af311b51e5e00
Khalimuha <kalimuha2016@gmail.com>
2025-05-16 07:10:05 +0300

    finalized

--- react-app-netflix-clone/src/Components/Footer/Footer.jsx
+++ react-app-netflix-clone/src/Components/Footer/Footer.jsx
@@ -1,67 +1,8 @@
-// import React from "react";
-// import "./footer.css";
-// import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-// import InstagramIcon from "@mui/icons-material/Instagram";
-// import YouTubeIcon from "@mui/icons-material/YouTube";
-
-// const Footer = () => {
-//   return (
-//     <div className="container-fluid">
-//       <div className="row pr-5 p-5">
-//         <div className="col-3">
-//           <div className="footer-icons gap-3">
-//             <FacebookOutlinedIcon />
-//             <InstagramIcon />
-//             <YouTubeIcon />
-//           </div>
-//           <div className="footer-data d-flex list-unstyled align-items-center">
-//             <div>
-//               <ul className="list-unstyled gap-3 text-secondary">
-//                 <li>Audio Description</li>
-//                 <li>Investor Relations</li>
-//                 <li>Legal Notice</li>
-//                 <li className="service-code">Service Code</li>
-//                 <li>&copy;1997-2025 netflix, Inc </li>
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="col-3 text-secondary">
-//           <ul className="list-unstyled gap-4">
-//             <li>Help Center</li>
-//             <li>Jobs</li>
-//             <li>Cookie Preference</li>
-//           </ul>
-//         </div>
-//         <div className="col-3 text-secondary">
-//           <ul className="list-unstyled gap-4">
-//             <li>Gift Cards</li>
-//             <li>Terms of Use</li>
-//             <li>Corporate Information</li>
-//           </ul>
-//         </div>
-//         <div className="col-3">
-//           <ul className="list-unstyled gap-4 text-secondary">
-//             <li>Media Center</li>
-//             <li>Privacy</li>
-//             <li>Contact Us</li>
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Footer;
-
-
-
 
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import "./footer1.css";
+import "./footer.css";
 
 const Footer = () => {
   return (
--- react-app-netflix-clone/src/Components/Header/Header.jsx
+++ react-app-netflix-clone/src/Components/Header/Header.jsx
@@ -1,60 +1,7 @@
-// import React from "react";
-// import "./header.css";
-// import netflixLogo from "../../assets/images/Netflix.png";
-
-// // import SearchIcon from "@mui/icons-material/Search";
-// import { IoSearch } from "react-icons/io5";
-// import { MdNotificationsNone } from "react-icons/md";
-// import { MdAccountBox } from "react-icons/md";
-// import { IoMdArrowDropdownCircle } from "react-icons/io";
-// // 
-// const Header = () => {
-//   return (
-//     <div className="row">
-//       <div className="header-left col-md-6 d-flex">
-//         <ul className="gap-4 d-flex align-items-center list-unstyled">
-//           <li>
-//             <img src={netflixLogo} alt="Netflix logo" width="100" />
-//           </li>
-//           <li>Home</li>
-//           <li>TV Shows</li>
-//           <li>Movies</li>
-//           <li>Latest</li>
-//           <li>My List</li>
-//           <li>Browse by Languages</li>
-//         </ul>
-//       </div>
-
-//       <div className="header-right col-md-6 d-flex justify-content-end">
-//         <ul className="gap-4 d-flex align-items-center list-unstyled">
-//           <li>
-//             {" "}
-//             <IoSearch />{" "}
-//           </li>
-//           <li>
-//             <MdNotificationsNone />
-//           </li>
-//           <li>
-//             <MdAccountBox />
-//           </li>
-//           <li>
-//             <IoMdArrowDropdownCircle />
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-
-
-
 
 import React from "react";
 import logo from "../../Assets/images/netflix.png";
-import "./header1.css";
+import "./header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
