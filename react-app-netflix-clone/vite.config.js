282b237f29f3e9406520ebf669b6c65438c4d72d
Khalimuha <kalimuha2016@gmail.com>
2025-05-19 09:31:24 +0300

    vite.config.js updated

--- react-app-netflix-clone/vite.config.js
+++ react-app-netflix-clone/vite.config.js
@@ -1,17 +1,17 @@
-// import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
+
+vite.config.js;
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "Netflix-Clone/"
+});
 
 // vite.config.js
 // import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
 // export default defineConfig({
-//   base: "Netflix-Clone/"
+//   base: "/Netflix-Clone/", // 🚨 Missing slash at the beginning
+//   plugins: [react()] // ✅ React plugin is required for React to work properly
 // });
-
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/Netflix-Clone/", // 🚨 Missing slash at the beginning
-  plugins: [react()] // ✅ React plugin is required for React to work properly
-});
